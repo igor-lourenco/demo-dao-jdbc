9578ca97301acdc2068f889427e9afbe414644b5
igor-lourenco <igor.lourencosantos@gmail.com>
2020-07-28 13:53:52 -0300

    Classe de entidade do departamento

---
+++ src/aplicacoes/Programa.java
@@ -0,0 +1,15 @@
+package aplicacoes;
+
+import modelo.entidades.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Departamento obj = new Departamento(1, "Livros");
+		
+		System.out.println(obj);
+
+	}
+
+}
