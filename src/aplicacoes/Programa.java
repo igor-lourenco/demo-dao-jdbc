df54cae6a0afcdc3b71b7bf9d37012b9d2bcd752
igor-lourenco <igor.lourencosantos@gmail.com>
2020-07-28 14:02:10 -0300

    Classe entidade do vendedor

--- src/aplicacoes/Programa.java
+++ src/aplicacoes/Programa.java
@@ -1,14 +1,18 @@
 package aplicacoes;
 
+import java.util.Date;
+
 import modelo.entidades.Departamento;
+import modelo.entidades.Vendedor;
 
 public class Programa {
 
 	public static void main(String[] args) {
 
 		Departamento obj = new Departamento(1, "Livros");
+		Vendedor vendedor = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
-		System.out.println(obj);
+		System.out.println(vendedor);
 
 	}
 
